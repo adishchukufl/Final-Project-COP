6522252fb425276c710e8863bffad1a29c410522
adishchukufl <73535315+adishchukufl@users.noreply.github.com>
2021-12-09 18:13:50 -0500

    Current

--- Final Project COP/Final Project COP.cpp
+++ Final Project COP/Final Project COP.cpp
@@ -4,6 +4,8 @@
 #include <iostream>
 #include <random>
 #include <vector>
+#include <queue>
+#include <unordered_map>
 
 using namespace std;
 
@@ -12,6 +14,7 @@ class WeightUndirectedGraph {
     vector<pair<int, int>> *adjListGraph; //the first value will be the vertex name, and THE second will be the weight
     int vertices;
     int numOfRoads = 0; 
+
 public:
     WeightUndirectedGraph(int numOfVertices) { //constructor for graph
         vertices = numOfVertices;
@@ -29,6 +32,44 @@ public:
         adjListGraph[vertexA].push_back(make_pair(vertexB, dWeight));
         adjListGraph[vertexB].push_back(make_pair(vertexA, dWeight));
         numOfRoads++;
+    }
+
+    void deleteBigEdge(int vertexA) { //helper function createdd for the smaller graph specifically
+        unordered_map <int, pair<int, int>> duplicates; //first is the index of the adjlist graph and second is the value of the weight
+        auto aList = adjListGraph[vertexA]; //vector of pairs
+        int count = 0;
+
+        for (int i = 0; aList.size() > i; i++) {
+            if (duplicates.find(aList[i].first) == duplicates.end()) { //if we found no copies
+                duplicates[aList[i].first] = make_pair(i, aList[i].second);
+            }
+            else {
+                if (duplicates[aList[i].first].second < aList[i].second) { //if the weight in our umap is less than the one the list, we delete i
+                    //if we find a lower weight we replace it 
+                    aList.erase(aList.begin() + i);
+                    i--;
+                    
+                }
+                else {
+                    aList.erase(aList.begin() + duplicates[aList[i].first].first - count);
+                    i--;
+                    if (i < aList.size()) {
+                        duplicates[aList[i].first] = make_pair(i, aList[i].second);
+                    }
+                    count++;
+                   
+                   
+                }
+
+            }
+            
+        }
+        vector <pair<int, int>> newVertexEdges;
+        for (auto a : duplicates) {
+            newVertexEdges.push_back(a.second);
+        }
+        adjListGraph[vertexA] = newVertexEdges;
+
     }
 
     bool checkEdge(int vertexA, int vertexB) { // we will be checking if an edge already exists
@@ -74,7 +115,54 @@ public:
         
     }
 
-    
+    vector<int> bellmanFord(int src) {
+        vector<int> distance;
+
+        for (int i = 0; i < vertices; i++) {
+            distance.push_back(INT_MAX);
+        }
+        distance[src] = 0;
+
+        //Number of Relaxations is equal to number of vertices
+        for (int x = 1; x <= vertices - 1; x++) {
+            //Double for loop iterates through AdjList, every single edge in the graph
+            for (int y = 0; y < vertices; y++) {
+                for (int z = 0; z < adjListGraph[y].size(); z++) {
+                    int secVertex = adjListGraph[y].at(z).first;
+                    int weight = adjListGraph[y].at(z).second;
+                    //Relaxation
+                    if (distance[y] != INT_MAX && distance[y] + weight < distance[secVertex])
+                        distance[secVertex] = distance[y] + weight;
+                }
+            }
+        }
+        return distance;
+    }
+
+    vector<int> dijkstra(int src) {
+        vector<int> distance(vertices, INT_MAX); //sets to infinity
+        priority_queue<pair<int, int>> pQueue;  //creates priority queue
+        pQueue.push({ distance[src] = 0, src }); //sets distance from itself to zero
+
+        while (!pQueue.empty()) {
+            int dist = pQueue.top().first;
+            int vertex = pQueue.top().second;
+            pQueue.pop();
+
+            if (dist > distance[vertex]) {
+                continue;
+            }
+
+            for (auto endVertex : adjListGraph[vertex]) {
+                if (distance[vertex] + endVertex.second < distance[endVertex.first]) {
+                    distance[endVertex.first] = distance[vertex] + endVertex.second;
+                    pQueue.push({ distance[endVertex.first], endVertex.first });
+                }
+            }
+        }
+        return distance;
+    }
+
 
     void printGraph() {
         for (int i = 0; vertices > i; i++) {
@@ -120,18 +208,57 @@ public:
     }
 };
 
+WeightUndirectedGraph smallerGraph(vector<int> deliveryLocations, WeightUndirectedGraph& g, bool bellmans) { //where 
+    WeightUndirectedGraph newGraph(deliveryLocations.size());
+    for (int i = 0; i < deliveryLocations.size(); i++) {
+        if (bellmans) { //we do bellmans to create our graphs
+            vector<int> results = g.bellmanFord(deliveryLocations[i]); 
+            for (int j = 1; j < deliveryLocations.size(); j++) {
+                newGraph.addEdge(i, j, deliveryLocations[j]);
+            }
+        }
+        else { //we  use djikstras
+            vector<int> results = g.dijkstra(deliveryLocations[i]);
+            for (int j = 1; j < deliveryLocations.size(); j++) {
+                newGraph.addEdge(i, j, deliveryLocations[j]);
+            }
+        }
+        
+    }
+
+    return newGraph;
+}
 
 
 int main()
 {
     int sizeOfCountry = 0;
+    int numOfVertices;
     cout << "*********Welcome to Triple A’s Delivery service*********" << endl;
     cout << "Enter the Size of your Country(Area in km^2): " << endl;
     cin >> sizeOfCountry;
+    if (sizeOfCountry < 10000) {
+        numOfVertices = 10000;
+    }
+    else if (sizeOfCountry < 30000) {
+        numOfVertices = 50000;
+    }
+    else if (sizeOfCountry < 50000) {
+        numOfVertices = 70000;
+    }
+    else {
+        numOfVertices = 100000;
+    }
 
-    WeightUndirectedGraph g(100);
+    WeightUndirectedGraph g(1000);
     g.generateGraph();
-    g.printGraph();
+    vector<int> loc = { 1,2,3,4 };
+    
+    WeightUndirectedGraph q = smallerGraph(loc, g, true);
+    q.printGraph();
+    q.deleteBigEdge(1);
+    q.printGraph();
+    
     
 }
 
